d351b2d3796f970f03c95564f530a76ba15da99f
unknown <tarpel.gta@gmail.com>
2025-07-04 15:52:43 +0300

    feat: add PostItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import ClassCounter from "./components/ClassCounter";
-
+import React, { useState } from "react";
+import'./styles/App.css';
+import { PostItem } from "./components/PostItem";
 
 function App() {
+  const [posts, setPostr] = useState([
+    {id:1, title:'JavaScript', body: 'Учимся подключать стили...'},
+    {id:2, title:'JavaScript', body: 'Учимся подключать стили...'},
+    {id:3, title:'JavaScript', body: 'Учимся подключать стили...'},
+  ])
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <h1 className="post-list__title">Список постов:</h1>
+      {posts.map(post => <PostItem post = {post} key = {post.id}/>)}
     </div>
   );
 }
