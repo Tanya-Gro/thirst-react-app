e0e86bb72a13f8e524c818099c4efbe1ed8707a6
unknown <tarpel.gta@gmail.com>
2025-07-04 12:03:12 +0300

    chore: environment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/counter";
+import { ManagedComponent } from "./components/Managed-component";
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
