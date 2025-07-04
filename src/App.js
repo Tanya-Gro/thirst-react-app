9d3bce9c2074874de9d2ba8fe2709e2877bea6ed
unknown <tarpel.gta@gmail.com>
2025-07-04 21:13:04 +0300

    feat: managed and unmanaget component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import'./styles/App.css';
 import { PostList } from "./components/PostList";
+import { MyButton } from "./components/UI/button/MyButton";
+import { MyInput } from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,8 +11,34 @@ function App() {
     {id:3, title:'JavaScript', body: 'Учимся подключать стили...'},
   ])
 
+  const [title, setTitle] = useState('');
+
+  // получение данных если компонент неуправляемый
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => { 
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+   }
+
   return (
     <div className="App">
+      {/* Управляемый компонент */}
+      <MyInput 
+        type="text" 
+        placeholder="Название поста" 
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      {/* Неуправляемый/неконтролируемый компонент */}
+      <MyInput 
+        type="text" 
+        placeholder="Описание поста"
+        ref={bodyInputRef}
+      />
+
+      <MyButton onClick={addNewPost}>Создать пост</MyButton>
       <PostList posts = {posts} title = "Список постов" />
     </div>
   );
