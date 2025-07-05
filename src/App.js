c389e96f6a5779c0ff9dbfad6d566a70a7cea576
unknown <tarpel.gta@gmail.com>
2025-07-05 10:36:08 +0300

    feat: add search posts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import'./styles/App.css';
 import { PostList } from "./components/PostList";
 import { PostForm } from "./components/PostForm";
 import { MySelect } from "./components/UI/select/MySelect";
+import { MyInput } from "./components/UI/input/MyInput";
 
 function App() {
 
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
   const [posts, setPosts] = useState([
     {id:1, title:'1JavaScript', body: 'Учимся подключать стили...3'},
     {id:2, title:'2JavaScript', body: 'Учимся подключать стили...2'},
     {id:3, title:'3JavaScript', body: 'Учимся подключать стили...1'},
   ]);
-  const [selectedSort, setSelectedSort] = useState('');
+  
+  const sortedPosts = useMemo(()=> {
+    console.log('Работает сортировка постов')
+    if(selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    }
+    return posts;
+  }, [selectedSort, posts]);
 
+  const sortedAndSearchedPosts = useMemo(
+    () => {
+      return sortedPosts.filter(post=> post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    },
+    [searchQuery, sortedPosts]
+  )
   const createPost = (newPost) => {     
     setPosts([...posts,  newPost ]); 
   }
@@ -22,16 +38,22 @@ function App() {
   }
 
   const sortPosts = (sort) => {    
-    console.log(sort) 
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]))); 
   }
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      
       <hr className="line"/>
       <div>
+        <MyInput 
+          type="text" 
+          placeholder="Поиск"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+        />
+        
         <MySelect
           defaultValue="Сортировка"
           options={[
@@ -42,11 +64,13 @@ function App() {
           onChange={sort =>sortPosts(sort)}
         />
       </div>
-      {posts.length
-        ? <PostList posts = {posts} title = "Список постов" remove = {removePost} />
-        : <h1 className="no-posts">
-            Посты не найдены
-          </h1>
+      {sortedAndSearchedPosts.length
+        ? 
+        <PostList posts = {sortedAndSearchedPosts} title = "Список постов" remove = {removePost} />
+        : 
+        <h1 className="no-posts">
+          Посты не найдены
+        </h1>
       }
     </div>
   );
