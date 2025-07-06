67a0ecc116c2f2459d4f59299ef1ec5c749f419d
unknown <tarpel.gta@gmail.com>
2025-07-06 12:42:27 +0300

    feat: add custom hook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { PostForm } from "./components/PostForm";
 import { PostFilter } from "./components/PostFilter";
 import { MyModal } from "./components/UI/MyModal/MyModal";
 import { MyButton } from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
 
@@ -16,20 +17,8 @@ function App() {
     {id:3, title:'3JavaScript', body: 'Учимся подключать стили...1'},
   ]);
   
-  const sortedPosts = useMemo(()=> {
-    console.log('Работает сортировка постов')
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(
-    () => {
-      return sortedPosts.filter(post=> post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    },
-    [filter.query, sortedPosts]
-  )
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  
   const createPost = (newPost) => {     
     setPosts([...posts,  newPost ]);
     setModal(false);
